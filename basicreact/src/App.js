80dc430a23d7a15e215b0db107001562f3d82fd5
utsav gupta <utsav.gupta@infochips.com>
2026-04-18 19:01:44 +0530

    basic project of react and vite with basic files

---
+++ basicreact/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      First Basic React Project
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ vite-basic-1/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Frst Vite basic project | Utsav Git</h1>
+    </>
+  )
+}
+
+export default App
